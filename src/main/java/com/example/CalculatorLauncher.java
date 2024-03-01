ba4cdb36a51023025bcd1caea56eeea339be23d4
WMaciejonczyk <268337@student.pwr.edu.pl>
2024-03-01 02:22:54 +0100

    Adding and subtracting methods

--- src/main/java/com/example/CalculatorLauncher.java
+++ src/main/java/com/example/CalculatorLauncher.java
@@ -4,4 +4,12 @@ public class CalculatorLauncher {
     public static void main(String[] args) {
         System.out.println("Coming soon!");
     }
+
+    public static int addNumbers(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    public static int subtractNumbers(int n1, int n2) {
+        return n1 - n2;
+    }
 }
